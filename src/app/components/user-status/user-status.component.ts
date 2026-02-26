be8bd3edca8871332c0555c32714652a5191f5f5
BNaugler317 <Brandon.Naugler@easternstudent.ca>
2026-02-26 13:36:52 -0400

    inital setup components generated

---
+++ src/app/components/user-status/user-status.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-status',
+  imports: [],
+  templateUrl: './user-status.component.html',
+  styleUrl: './user-status.component.css'
+})
+export class UserStatusComponent {
+
+}
